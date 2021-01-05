4de61971cd8370a848a2e5d56151d61d2e620ef2
Maximos Salzman <maximossalzman@gmail.com>
2021-01-05 09:35:08 -0500

    initial commit

---
+++ app/routes_mockup.py
@@ -0,0 +1,22 @@
+
+## user login
+
+## user sign up
+
+
+## Search for Location Suggestions
+    ## return list of suggested autocomplete cities
+
+## Search for Location for businesses
+    ## search yelp for location
+    ## need to search database to see if business exists in database yet
+    ## if it does exist in database return with reviews
+    ## if it does not exist in database create a new business in database
+    ## return list of businesses
+
+## get business and it's reviews
+    ## search for businessId in database and return all it's reviews
+
+## post review
+    ## receive data from create review form
+    ## save data in the database
---
+++ componentsList.js
@@ -0,0 +1,18 @@
+// NavBar
+    // search bar
+    // logo
+    // dropdown options
+
+//Map
+    // scalable
+
+// Business Cards
+    // stars
+    // highlighted review
+    // business info
+
+// Business Profile page
+
+// Profile page
+
+// Leave a review Form
