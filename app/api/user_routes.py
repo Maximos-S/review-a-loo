9ecc9b9fe9e14f4f333b92e9bd3574e986075157
Maximos Salzman <maximossalzman@gmail.com>
2021-01-26 19:11:19 -0500

    fixes git ignore

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, jsonify
 import boto3
 import uuid
-from flask_login import login_required
-from app.models import User, current_user
+from flask_login import login_required, current_user
+from app.models import User
 from app.forms import EditUserForm
 
 user_routes = Blueprint('users', __name__)
@@ -19,7 +19,7 @@ def user(id):
     return user.to_dict_reviews()
 
 ## edit user
-@user_routes.rout('/<int:id>', methods=['POST'])
+@user_routes.route('/<int:id>', methods=['POST'])
 def edit_user(id):
     if id == 1:
         return {"error": "not authorized"}
@@ -29,4 +29,6 @@ def edit_user(id):
     form = EditUserForm()
     if form.validate_on_submit():
         if form.data['image']:
+            s3 = boto3.resource('s3')
+            # uploaded_image = s3.Bucket('review-a-loo-profile-image').put_object(Key=)
 
--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -2,3 +2,4 @@ from .login_form import LoginForm
 from .signup_form import SignUpForm
 from .business_search import RegionSearchForm
 from .review_form import ReviewForm
+from .edit_user_form import EditUserForm
